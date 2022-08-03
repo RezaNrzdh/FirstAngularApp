432ee0f9b48c79d58a0310c7909f3ac177458202
RezaNrzdh <reza.nrzdh@gmail.com>
2022-08-03 15:50:10 +0430

    'First Commit'

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-register',
+    template: `
+        <h1>Register Component</h1>
+        <h3>H3 Tag</h3>
+        `,
+})
+export class RegisterComponent {
+
+}
