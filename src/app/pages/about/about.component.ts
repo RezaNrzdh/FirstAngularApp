8de3f9bf9186c9521f55d8b5a0dc70167aa4bbd5
Reza Norouzzadeh <reza.nrzdh@gmail.com>
2022-08-05 01:09:51 +0430

    update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { About } from './about.model';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  colleague: Array<About> = [
+    new About('Reza Norouzzadeh', 'UIUX desginer & Web developer'),
+    new About('Mobina Khshnood', 'UIUX Designer'),
+    {name: 'test', job: 'test' }
+  ];
 
 }
---
+++ src/app/pages/about/about.model.ts
@@ -0,0 +1,9 @@
+export class About {
+    public name: string;
+    public job: string;
+
+    constructor (name: string, job: string){
+        this.name = name;
+        this.job = job;
+    }
+}
