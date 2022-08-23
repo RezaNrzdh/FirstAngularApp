bbeb1c096aaa89d7bb370f22a0c16104d682f33e
RezaNrzdh <reza.nrzdh@gmail.com>
2022-08-23 13:24:56 +0430

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() itemContent = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Title1', 'lorem ipsum dolor esmit', 'https://via.placeholder.com/150/5e12c6'),
-    new Recipe('Title2', 'lorem ipsum dolor esmit 2', 'https://via.placeholder.com/150/5e12c6')
+    new Recipe('Ghormeh sabzi Recipe', 'lorem ipsum dolor esmit', 'https://via.placeholder.com/150/5e12c6'),
+    new Recipe('Title2', 'lorem ipsum dolor esmit', 'https://via.placeholder.com/150/5e12c6')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getItemContent = (content: Recipe) => {
+    this.itemContent.emit(content);
   }
 
 }
