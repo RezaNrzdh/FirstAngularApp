ed4564bd1c84cc78b8d0499351537b2bc5fb7602
RezaNrzdh <reza.nrzdh@gmail.com>
2022-08-27 09:11:31 +0430

    update

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredientList = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddHandler = (nameInput: HTMLInputElement, amountInput: any) => {
+    this.newIngredientList.emit(new Ingredient(nameInput.value, amountInput.value));
   }
 
 }
